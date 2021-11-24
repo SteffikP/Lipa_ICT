67115d67a50d4e8192cbd12773ff9d4ef497497b
SteffikP <80819418+SteffikP@users.noreply.github.com>
2021-11-24 16:07:17 +0100

    Add files via upload

---
+++ LCD/LCD_RGB_jedno_tlacitko.ino
@@ -0,0 +1,45 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8,A4,2,3,4,7);
+int SWR = A1;
+int SWG = A2;
+int SWB = A3;
+
+int LEDR = 5;
+int LEDG = 6;
+int LEDB = 9;
+
+byte r, g, b;
+void setup()
+{
+  pinMode(SWR,INPUT);
+  pinMode(SWG,INPUT);
+  pinMode(SWB,INPUT);
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  analogWrite(LEDR, 128);
+  analogWrite(LEDG, 128);
+  analogWrite(LEDB, 128);
+  lcd.begin(16,2);
+  lcd.print("REGULACE BAREV");
+  lcd.setCursor(0,1);
+  lcd.print("R128 G128 B128");
+  r = 128;
+  g = 128;
+  b = 128;
+}
+
+void loop()
+{
+  if(digitalRead(SWR))
+  {
+    analogWrite(LEDR, ++r);
+    lcd.setCursor(1,1);
+    lcd.print("   ");
+    lcd.setCursor(1,1);
+    lcd.print(r);
+  }
+  
+  delay(100);
+}
