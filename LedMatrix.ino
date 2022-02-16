3e4b8eaa6afacaec942ae4a198e2cb9dc4079dda
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2022-02-16 17:17:57 +0100

    Create LedMatrix.ino

---
+++ LedMatrix.ino
@@ -0,0 +1,57 @@
+#include "LedControl.h"
+
+/*
+ Now we need a LedControl to work with.
+ ***** These pin numbers will probably not work with your hardware *****
+ pin 12 is connected to the DataIn 
+ pin 11 is connected to the CLK 
+ pin 10 is connected to LOAD 
+ We have only a single MAX72XX.
+ */
+LedControl lc=LedControl(11,13,12,1);
+
+/* we always wait a bit between updates of the display */
+unsigned long delaytime=100;
+
+void setup() {
+  /*
+   The MAX72XX is in power-saving mode on startup,
+   we have to do a wakeup call
+   */
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,8);
+  /* and clear the display */
+  lc.clearDisplay(0);
+}
+
+/*
+ This method will display the characters for the
+ word "Arduino" one after the other on the matrix. 
+ (you need at least 5x7 leds to see the whole chars)
+ */
+void writeArduinoOnMatrix() {
+  /* here is the data for the characters */
+  byte a[5]={B01111110,B10001000,B10001000,B10001000,B01111110};
+  byte r[5]={B00111110,B00010000,B00100000,B00100000,B00010000};
+  byte d[5]={B00011100,B00100010,B00100010,B00010010,B11111110};
+  byte u[5]={B00111100,B00000010,B00000010,B00000100,B00111110};
+  byte i[5]={B00000000,B00100010,B10111110,B00000010,B00000000};
+  byte n[5]={B00111110,B00010000,B00100000,B00100000,B00011110};
+  byte o[5]={B00011100,B00100010,B00100010,B00100010,B00011100};
+
+  /* now display them one by one with a small delay */
+  lc.setRow(0,0,a[0]);
+  lc.setRow(0,1,a[1]);
+  lc.setRow(0,2,a[2]);
+  lc.setRow(0,3,a[3]);
+  lc.setRow(0,4,a[4]);
+  delay(delaytime);
+}
+
+void loop() { 
+  writeArduinoOnMatrix();
+  delay(1000);
+  lc.clearDisplay(0);
+  delay(1000);
+}
